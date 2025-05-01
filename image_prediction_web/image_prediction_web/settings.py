2e50d1dd98c2c9756048ce656a33cfdcb004c210
nukhugama <nukhugama20@gmail.com>
2025-05-01 20:16:11 +0900

    Edit Load Models

--- image_prediction_web/image_prediction_web/settings.py
+++ image_prediction_web/image_prediction_web/settings.py
@@ -118,6 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# Optional but helpful for development
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'predictor' 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
