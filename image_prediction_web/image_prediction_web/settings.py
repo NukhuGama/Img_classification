e571e144581fdc21955812bdc0a1cf8218d44667
nukhugama <nukhugama20@gmail.com>
2025-05-04 22:32:26 +0900

    Working on dev S3 Ver0

--- image_prediction_web/image_prediction_web/settings.py
+++ image_prediction_web/image_prediction_web/settings.py
@@ -29,8 +29,10 @@ DEBUG = True
 # ALLOWED_HOSTS = []
 # import os
 
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+# ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 # ALLOWED_HOSTS = ['img-classification.onrender.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'img-classification.onrender.com']
+
 
 
 
